6266163032b258b66925eb614d059aab017fee85
Ahmad Ahsan <ahsankhan123336@gmail.com>
2025-07-28 13:32:12 +0500

    Add files via upload

---
+++ CruidApp/Model/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CruidApp.Model
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+
+
+    }
+}
